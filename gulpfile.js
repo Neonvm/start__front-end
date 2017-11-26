26b58caee43e7e69c3ce9365cd50e0ac2026c5c7
Vladuslav <ngvmtext@gmail.com>
2017-11-26 16:59:08 +0200

    gulp-2017

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,26 @@ const 	gulp = require('gulp'),
 		watch = require('gulp-watch'),
     	prefixer = require('gulp-autoprefixer'), 
     	rigger = require('gulp-rigger'),
-	  	sass = require('gulp-sass');
+        sass = require('gulp-sass'),
+        browserSync = require('browser-sync')
+        babel = require('gulp-babel');
 
+var reload = browserSync.reload;
 var path = {
     build: {
         html: 'build/',
-        css: 'build/css/'
+        css: 'build/css/',
+        js:'build/js/'
     },
     src: {
         html: 'src/*.html',
-        style: 'src/style/main.scss'
+        style: 'src/style/main.scss',
+        js:'src/js/*.js'
     },
     watch: {
         html: 'src/**/*.html',
         style: 'src/style/**',
+        js:'src/js/**/*.js'
     },
     clean: './build'
 };
@@ -24,7 +30,8 @@ var path = {
 gulp.task('html:build', function () {
     gulp.src(path.src.html) 
         .pipe(rigger())
-        .pipe(gulp.dest(path.build.html));
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream:true}));
 });
 
 gulp.task('style:build', function () {
@@ -35,11 +42,33 @@ gulp.task('style:build', function () {
         }))
         .pipe(prefixer())
         .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream:true}));
 });
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) 
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('browserSync', function() {
+    browserSync({
+      server: {
+        baseDir: "build/",
+        directory: true
+      },
+      port: 8080,
+      open: true,
+      notify: true
+    });
+  });
+
 
 gulp.task('build', [
     'html:build',
-    'style:build'
+    'style:build',
+    'js:build'
 ]);
 
 gulp.task('watch', function(){
@@ -49,6 +78,9 @@ gulp.task('watch', function(){
     watch([path.watch.style], function(event, cb) {
         gulp.start('style:build');
     });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'watch', 'browserSync']);
